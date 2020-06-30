375b95d0b326b0de80f3ed79f0890aac483c40bf
gskidmor <gskidmor@ucsc.edu>
2020-06-30 16:01:35 -0700

    After understanding the process a bit better, I have moved back to editing my original doc. I updated the syntax to be more continuous with standard pytorch syntax. While running it, there are no errors but it still doesn't perform properly

--- NN_DigitClassification_Attempt2.py
+++ NN_DigitClassification_Attempt2.py
@@ -1,18 +1,17 @@
 import torch
-import torchvision
+import numpy as np
 import torch.nn as nn
-import torch.optim
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
 
 dtype = torch.float
 # Running on the gpu; to change to cpu, switch "cuda:0" to "cpu"
-device = torch.device("cuda:0")
+device = torch.device("cpu")
 # Creating a debug variable to prevent unnecssary printing
 DEBUG = False
 
-# Getting the data from the MNIST database
-datapath = "MNIST/"
-train_data = np.loadtxt(datapath+"mnist_train.csv", delimiter=",")
-test_data = np.loadtxt(datapath+"mnist_test.csv", delimiter=",")
+# Setting parameters
 num_images = 60000
 # Size of one image
 input_size = 784
@@ -21,21 +20,60 @@ hidden_sizes = [128, 64]
 # Size of the output vector
 output_size = 10
 
-# Creating matrices that hold the images
-# Each image is stored in a row with the first column holding the correct label
-img_in = torch.tensor(train_data[:, 1:], device=device, dtype=dtype) * mapping + 0.01
-img_label = torch.tensor(train_data[:, :1], device=device, dtype=dtype)
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)), ])
 
-# Setting params for the weights
-H, Num_In_Nodes, Num_Out_Nodes = 100, (28*28), 10
-
-# Initializing the weights to random values
-# Specifically turning on the requires_grad to true so we can backprop
-weight1 = torch.randn(H, Num_In_Nodes, device=device, dtype=dtype, requires_grad=True)
-weight2 = torch.randn(Num_Out_Nodes, H, device=device, dtype=dtype, requires_grad=True)
+# Downloading and loading in the image data from the MNIST website
+trainset = datasets.MNIST('./MNIST_Train', download=True, train=True, transform=transform)
+valset = datasets.MNIST('./MNIST_Test', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 
 # Setting params for the nn training
 # Defines how how much the network learns from each iteration
-learning_rate = 1e-1
+learning_rate = 1e-3
 # How many iterations the system goes through
 num_iter = 1
+
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.LogSoftmax(dim=1))
+
+# Setting up the optimizer
+optimizer = optim.SGD(model.parameters(), learning_rate, momentum=0.9)
+
+# Training the network on our input images
+for t in range(num_iter):
+    iter_counter = 0
+    for images, labels in trainloader:
+        images = images.view(images.shape[0], -1)
+
+        # Reset the gradients
+        optimizer.zero_grad()
+
+        # Now pass the input to the model for training
+        nn_guess = model(images)
+        nn_guess = nn.functional.log_softmax(nn_guess, dim=1)
+
+        # Computing the loss function with built-in negative log-likelihood function
+        loss = nn.functional.nll_loss(nn_guess, labels)
+        print(nn_guess)
+        print(labels)
+
+        # Use autograd to compute the gradients of the weights
+        loss.backward()
+
+        if iter_counter % 63 == 0:
+            print()
+            print("Image Number:")
+            print(iter_counter)
+            print("Loss:")
+            print(loss.item())
+            print()
+
+        iter_counter += 1
+
+        # Update the optim's weights
+        optimizer.step()
--- NumberImageNN.py
+++ NumberImageNN.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import torch.nn as nn
+import torch.optim as optim
 
 dtype = torch.float
 # Running on the gpu; to change to cpu, switch "cuda:0" to "cpu"
@@ -21,134 +23,66 @@ img_in = torch.tensor(train_data[:, 1:], device=device, dtype=dtype) * mapping +
 img_label = torch.tensor(train_data[:, :1], device=device, dtype=dtype)
 
 # Setting params for the weights
-H, Num_In_Nodes, Num_Out_Nodes = 100, (28*28), 10
-
-# Initializing the weights to random values
-# Specifically turning on the requires_grad to true so we can backprop
-weight1 = torch.randn(H, Num_In_Nodes, device=device, dtype=dtype, requires_grad=True)
-weight2 = torch.randn(Num_Out_Nodes, H, device=device, dtype=dtype, requires_grad=True)
+H1, H2, Num_In_Nodes, Num_Out_Nodes, Batch_Size = 100, 50, (28*28), 10, 64
 
 # Setting params for the nn training
 # Defines how how much the network learns from each iteration
-learning_rate = 1e-1
+learning_rate = 1e-2
 # How many iterations the system goes through
 num_iter = 1
+# Creating matrices to store the results of the nn_guess and labels
+guess_set = torch.zeros(Batch_Size, Num_Out_Nodes, device=device, dtype=dtype)
+label_set = torch.zeros(Batch_Size, device=device, dtype=torch.int64)
+batch_index = 0
+
+model = nn.Sequential(
+    nn.Linear(Num_In_Nodes, H1).cuda(),
+    nn.ReLU().cuda(),
+    nn.Linear(H1, H2).cuda(),
+    nn.ReLU().cuda(),
+    nn.Linear(H2, Num_Out_Nodes).cuda()
+)
+
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
 ############################################################################################
 # Running the training of the neural network
 for t in range(num_iter):
     for i in range(len(img_in)):
-        # The network makes a guess based on each image and the weights
-        # Implements a ReLU with clamp(min=0) so that we zero out any negative results
+        optimizer.zero_grad()
 
-        # First clones the input image row and transposes it
-        hidden_node = img_in[i].clone()
-        hidden_node = torch.transpose(hidden_node, -1, 0)
+        nn_guess = model(img_in[i])
 
-        if DEBUG:
-            # Printing out dimensions for the first dot product
-            # Expected sizes: weight1 = 100 x 784; img_in[i] = 784 x 1
-            print("Weight 1 Dimensions")
-            try:
-                print(len(weight1), len(weight1[0]))
-            except TypeError:
-                print(len(weight1), 1)
+        # Storing the guess and correct result into matrices for loss computation
+        guess_set[batch_index] = nn_guess
+        label_set[batch_index] = img_label[i]
 
-            print("Hidden Node Dimensions")
-            try:
-                print(len(hidden_node), len(hidden_node[0]))
-            except TypeError:
-                print(len(hidden_node), 1)
+        # Calculating loss and updating weights after each minibatch
+        if batch_index == 63:
+            if DEBUG:
+                print(guess_set)
+                print(label_set.t())
 
-        # Then calculates the intermediate guess based on weight1
-        # The resulting matrix should be of size 100 x 1
-        intermediate_guess = torch.matmul(weight1, hidden_node).clamp(min=0, max=1)
+            # Calculating loss of the most recent batch
+            loss = nn.functional.nll_loss(nn.LogSoftmax(guess_set), label_set.t())
+            loss.backward(retain_graph=True)
+            optimizer.step()
 
-        if DEBUG:
-            # Printing out dimensions for the first dot product
-            # Expected sizes: weight2 = 10 x 100; intermediate_guess = 100 x 1
-            print("Weight 2 Dimensions")
-            try:
-                print(len(weight2), len(weight2[0]))
-            except TypeError:
-                print(len(weight2), 1)
+            if DEBUG:
+                print()
+                print("Resulting Error")
+                print(i, loss)
+                print()
 
-            print("Intermediate Guess Dimensions")
-            try:
-                print(len(intermediate_guess), len(intermediate_guess[0]))
-            except TypeError:
-                print(len(intermediate_guess), 1)
+            # Prints out the loss every 50 iterations
+            if i % 1000 == 0:
+                print()
+                print("Iter:")
+                print(i)
+                print("Loss:")
+                print(loss)
+                print()
 
-        # Then calculates the final neural network guess using the intermediate guess and
-        # the second weight
-        nn_guess = torch.matmul(weight2, intermediate_guess).clamp(min=0, max=1)
-
-        if DEBUG:
-            # Printing out dimensions for the final result
-            # Expected size: nn_guess = 10 x 1
-            print("Neural Network Guess Dimensions")
-            try:
-                print(len(nn_guess), len(nn_guess[0]))
-            except TypeError:
-                print(len(nn_guess), 1)
-
-        # Creating a one_hot array to calculate the error and loss
-        # The expected result should be an array of all zeros except for a
-        # single 1 in the img_label[i]'s digit location
-        num_array = [*range(0, 10, 1)]
-        for index in range(10):
-            num_array[index] = int(num_array[index] == img_label[i])
-        correct_result = torch.tensor(num_array, device=device, dtype=dtype, requires_grad=True)
-
-        if DEBUG:
-            # Printing out dimensions for the correct_result
-            # Expected size: nn_guess = 10 x 1
-            print("Correct Result Dimensions")
-            try:
-                print(len(correct_result), len(correct_result[0]))
-            except TypeError:
-                print(len(correct_result), 1)
-
-        # Calculating the error based on the nn_guess and the provided label
-        error_vector = nn_guess - correct_result
-        errors = error_vector.pow(2).sum()
-
-        if DEBUG:
-            print()
-            print("Resulting Error")
-            print(i, errors)
-            print()
-
-        # Prints out the loss every 50 iterations
-        if i % 1000 == 0:
-            print()
-            print("Iter:")
-            print(i)
-            print("Correct Result:")
-            print(correct_result)
-            print("NN Guess")
-            print(nn_guess)
-            print()
-
-        # Backprop with pyTorch for stochastic gradient descent
-        errors.backward()
-
-        if DEBUG:
-            print("Weight1 and Weight2 grad")
-            print(weight1.grad, weight2.grad)
-            print()
-
-        # Need to wrap with torch.no_grad() so that this operation is not tracked by autograd
-        with torch.no_grad():
-            # Update our weights based on the backprop generated by our loss function
-            # print(weight1.grad)
-            # print(weight2.grad)
-            # print()
-            weight1 -= learning_rate * weight1.grad
-            weight2 -= learning_rate * weight2.grad
-
-            # Reset the gradients after updating the weights
-            weight1.grad.zero_()
-            weight2.grad.zero_()
-            errors = 0
+            else:
+                batch_index = batch_index + 1
 #loop
